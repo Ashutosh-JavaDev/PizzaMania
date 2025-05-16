96d5920a23e9ec5ca22f78158f36e339cbe36ed8
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-17 04:24:12 +0530

    Added Database Connection

--- Project/src/main/Java/App/LoginPage.java
+++ Project/src/main/Java/App/LoginPage.java
@@ -10,10 +10,15 @@ import java.util.Scanner;
 import java.awt.Image;
 
 public class LoginPage extends JFrame implements ActionListener {
+    JButton button, siggnup;
+    JTextField mailfield;
+    JPasswordField passbox;
+
     public LoginPage() {
         setTitle("Login Page");
         try {
-            File file = new File("/home/ashutosh/Desktop/PizzaMania/PizzaMania/Project/src/main/Java/App/Images/0238e1d8-09ae-4bfd-a506-edd260a59d1c-removebg-preview.png");
+            File file = new File(
+                    "/home/ashutosh/Desktop/PizzaMania/PizzaMania/Project/src/main/Java/App/Images/0238e1d8-09ae-4bfd-a506-edd260a59d1c-removebg-preview.png");
             if (!file.exists()) {
                 System.out.println("Image file not found!");
             }
@@ -25,25 +30,25 @@ public class LoginPage extends JFrame implements ActionListener {
             JLabel label = new JLabel(i3);
             label.setBounds(50, 10, 100, 100);
             add(label);
-           
+
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
         JLabel title = new JLabel("Pizza Mania");
-        Font tit=new Font("Arial",Font.BOLD,20);
+        Font tit = new Font("Arial", Font.BOLD, 20);
         title.setFont(tit);
         title.setBounds(150, 70, 200, 20);
         add(title);
         JLabel head = new JLabel("Welcome to the Pizza Mania....");
-        Font headfont=new Font("Arial",Font.ITALIC,16);
+        Font headfont = new Font("Arial", Font.ITALIC, 16);
         head.setFont(headfont);
-        head.setBounds(130,100, 220, 20);
+        head.setBounds(130, 100, 220, 20);
         add(head);
         // // Email
         JLabel mail = new JLabel("Email:");
         Font font = new Font("Arial", Font.BOLD, 20);
         mail.setFont(font);
-        mail.setBounds(80,150, 70, 20);
+        mail.setBounds(80, 150, 70, 20);
         add(mail);
         // Password
         JLabel password = new JLabel("Password:");
@@ -52,24 +57,24 @@ public class LoginPage extends JFrame implements ActionListener {
         password.setBounds(80, 200, 120, 20);
         add(password);
         // Mail Box
-        JTextField mailfield = new JTextField();
-        mailfield.setBounds(110,180, 150, 20);
+        mailfield = new JTextField();
+        mailfield.setBounds(110, 180, 150, 20);
         add(mailfield);
         // Password Box
-        JPasswordField passbox = new JPasswordField();
+        passbox = new JPasswordField();
         passbox.setBounds(110, 230, 150, 20);
         add(passbox);
         // Button
-        JButton button=new JButton("Login");
-        button.setBounds(300,270,80,40);
-        Font buttonFont=new Font("Arial",Font.BOLD,14);
+        button = new JButton("Login");
+        button.setBounds(300, 270, 80, 40);
+        Font buttonFont = new Font("Arial", Font.BOLD, 14);
         button.setFont(buttonFont);
         add(button);
         button.addActionListener(this);
         // DSignup
-        JButton siggnup=new JButton("Signup");
-        siggnup.setBounds(100,270,100,40);
-        Font signupfont=new Font("Arial",Font.BOLD,14);
+        siggnup = new JButton("Signup");
+        siggnup.setBounds(100, 270, 100, 40);
+        Font signupfont = new Font("Arial", Font.BOLD, 14);
         siggnup.setFont(signupfont);
         add(siggnup);
         siggnup.addActionListener(this);
@@ -82,8 +87,13 @@ public class LoginPage extends JFrame implements ActionListener {
 
     }
 
+    @Override
     public void actionPerformed(ActionEvent ae) {
-
+        if (ae.getSource() == button) {
+            String emmailID = mailfield.getText();
+            String password = passbox.getText();
+            String query = "select*from LoginPage where Email='" + emmailID + "' and Password='" + password + "'";
+        }
     }
 
     public static void main(String[] args) {
