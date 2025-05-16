c7cf0e44f6bdb4fc5dde2a7a5f35f53a55016f5d
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-17 00:27:44 +0530

    adding frame in file

--- Project/src/main/Java/App/LoginPage.java
+++ Project/src/main/Java/App/LoginPage.java
@@ -1,19 +1,33 @@
 package main.Java.App;
+
 import javax.swing.*;
+
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-public class LoginPage extends JFrame implements ActionListener{
-    void loginpage(){
+public class LoginPage extends JFrame implements ActionListener {
+    public LoginPage() {
+        setTitle("Login Page");
+        JLabel li = new JLabel("Email:");
+        Font font = new Font("Arial", Font.BOLD, 20);
+        li.setFont(font);
+        li.setBounds(20, 20,70, 20);
+        add(li);
         setLayout(null);
-        setSize(300, 400);
-        setLocation(300, 0);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setUndecorated(true);
+        setSize(500, 400);
+        setLocation(300, 300);
         setVisible(true);
-    }
-    public void actionPerformed(ActionEvent ae){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+
+    }
+
+    public static void main(String[] args) {
+        new LoginPage();
     }
 }
