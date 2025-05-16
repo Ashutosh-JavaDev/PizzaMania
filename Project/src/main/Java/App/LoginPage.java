345525ef30f74a7e58650e732b89bb370e21956b
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-16 23:34:35 +0530

    Adding File in Java

--- Project/src/main/Java/App/LoginPage.java
+++ Project/src/main/Java/App/LoginPage.java
@@ -4,6 +4,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-public class LoginPage extends JFrame{
-    
+public class LoginPage extends JFrame implements ActionListener{
+    void loginpage(){
+        setLayout(null);
+        setSize(300, 400);
+        setLocation(300, 0);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setUndecorated(true);
+        setVisible(true);
+    }
+    public void actionPerformed(ActionEvent ae){
+
+    }
 }
