08fe7927d808912e632cd8b0442fab24d79bc50b
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-17 00:37:45 +0530

    Adding mail and password section

--- Project/src/main/Java/App/LoginPage.java
+++ Project/src/main/Java/App/LoginPage.java
@@ -10,11 +10,27 @@ import java.util.Scanner;
 public class LoginPage extends JFrame implements ActionListener {
     public LoginPage() {
         setTitle("Login Page");
-        JLabel li = new JLabel("Email:");
+        // Email
+        JLabel mail = new JLabel("Email:");
         Font font = new Font("Arial", Font.BOLD, 20);
-        li.setFont(font);
-        li.setBounds(20, 20,70, 20);
-        add(li);
+        mail.setFont(font);
+        mail.setBounds(20, 20,70, 20);
+        add(mail);
+        // Password
+        JLabel password = new JLabel("Password:");
+        Font pass = new Font("Arial", Font.BOLD, 20);
+        password.setFont(pass);
+        password.setBounds(20, 60,120, 20);
+        add(password);
+        // Mail Box
+        JTextField mailfield=new JTextField();
+        mailfield.setBounds(110,20,150,20);
+        add(mailfield);
+        // Password  Box
+        JPasswordField passbox=new JPasswordField();
+        passbox.setBounds(150,60,150,20);
+        add(passbox);
+        // Frame Creation
         setLayout(null);
         setSize(500, 400);
         setLocation(300, 300);
