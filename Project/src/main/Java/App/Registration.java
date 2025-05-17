b3c2c41b4eb3b3cfd30e56bd5771f40d5e5a92e5
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-18 00:30:59 +0530

    Picture  added

--- Project/src/main/Java/App/Registration.java
+++ Project/src/main/Java/App/Registration.java
@@ -1,12 +1,9 @@
 package main.Java.App;
 import javax.swing.*;
-import java.sql.SQLException;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.sql.ResultSet;
-import java.util.Scanner;
 import java.awt.Image;
 import java.awt.BorderLayout;
 public class Registration extends  JFrame implements ActionListener {
