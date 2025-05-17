ee86d8d93de152a6493bd0cf3017b9e9c6dfa6f3
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-17 21:56:18 +0530

    Creating  Registration Form

--- Project/src/main/Java/App/Registration.java
+++ Project/src/main/Java/App/Registration.java
@@ -10,6 +10,12 @@ import java.util.Scanner;
 import java.awt.Image;
 public class Registration extends  JFrame implements ActionListener {
     public Registration(){
+        setTitle("Registration Form");
+        setLayout(null);
+        setSize(500,500);
+        setLocation(400, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
 
     }
     public void actionPerformed(ActionEvent ae){
