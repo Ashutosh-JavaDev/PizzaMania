58af271afd4b864c67204a5a12af99ab6cccea49
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-20 01:26:17 +0530

    Normal  pizza Page Created

--- Project/src/main/Java/App/NormalPizza.java
+++ Project/src/main/Java/App/NormalPizza.java
@@ -9,9 +9,16 @@ import java.awt.Image;
 import java.awt.BorderLayout;
 public class NormalPizza extends JFrame  implements ActionListener{
     public NormalPizza(){
-
+        setLayout(null);
+        setSize(700, 1000);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
     }
     public void actionPerformed(ActionEvent ae){
 
+    }
+    public static void main(String[] args) {
+        new NormalPizza();
     }
 }
