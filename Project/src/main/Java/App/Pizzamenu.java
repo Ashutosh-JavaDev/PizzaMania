a75d080519aca3f8c444064cf031ec7f0b01568d
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-22 10:56:44 +0530

    Added font the project

--- Project/src/main/Java/App/Pizzamenu.java
+++ Project/src/main/Java/App/Pizzamenu.java
@@ -1,4 +1,5 @@
 package main.Java.App;
+
 import javax.swing.*;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -7,24 +8,32 @@ import java.io.File;
 import java.sql.PreparedStatement;
 import java.awt.Image;
 import java.awt.BorderLayout;
-public class Pizzamenu extends JFrame  implements ActionListener{
+
+public class Pizzamenu extends JFrame implements ActionListener {
     JLabel Pizzaname;
-    JCheckBox type=new JCheckBox("'Veg Pizza','Non-Veg Pizza','Delux Veg  Pizza','Non-Veg Delux'");
-    public Pizzamenu(){
+    String[] list = { "Veg Pizza", "Non-Veg Pizza", "Delux Veg  Pizza", "Non-Veg Delux" };
+    JComboBox<String> type = new JComboBox<>(list);
+
+    public Pizzamenu() {
         setTitle("Pizza Mania");
-        Pizzaname=new JLabel("Pizza Name");
-        
-        Pizzaname.setBounds(10, 50, 150, 30);
+        Pizzaname = new JLabel("Pizza Name");
+        Font pizzaFont = new Font("Arial", Font.BOLD, 18);
+        Pizzaname.setFont(pizzaFont);
+        Pizzaname.setBounds(30, 50, 150, 30);
         add(Pizzaname);
+        type.setBounds(150, 30, 150, 30);
+        add(type);
         setLayout(null);
-        setSize(1000,700);
+        setSize(1000, 700);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
-    public void actionPerformed(ActionEvent ae){
+
+    public void actionPerformed(ActionEvent ae) {
 
     }
+
     public static void main(String[] args) {
         new Pizzamenu();
     }
