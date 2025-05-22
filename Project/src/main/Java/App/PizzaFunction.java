fc8d8c218ebaaf5e4c52baf9b4e7d57aaa86b80f
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-22 23:52:43 +0530

    Pizza mania main page loaded

---
+++ Project/src/main/Java/App/PizzaFunction.java
@@ -0,0 +1,19 @@
+package main.Java.App;
+
+class VegPizza {
+    public int Vegpizza = 300;
+    public int nonVegpizza = 400;
+    public int DeluxVegpizza = 550;
+    public int DeluxNonVegpizza = 650;
+
+    public int VegpizzaCalculate() {
+        int totalVeg=0;
+        
+        return totalVeg;
+
+    }
+}
+
+public class PizzaFunction {
+
+}
