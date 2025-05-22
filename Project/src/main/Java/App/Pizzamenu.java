35de49044ac8b798538caadffb9c85f0988dd0ae
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-22 11:10:02 +0530

    Added font the project

--- Project/src/main/Java/App/Pizzamenu.java
+++ Project/src/main/Java/App/Pizzamenu.java
@@ -1,10 +1,12 @@
 package main.Java.App;
 
+import javax.management.RuntimeErrorException;
 import javax.swing.*;
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.io.InputStream;
 import java.sql.PreparedStatement;
 import java.awt.Image;
 import java.awt.BorderLayout;
@@ -15,19 +17,39 @@ public class Pizzamenu extends JFrame implements ActionListener {
     JComboBox<String> type = new JComboBox<>(list);
 
     public Pizzamenu() {
+
         setTitle("Pizza Mania");
+        // title
+        JLabel label = new JLabel("Welcome to Pizza Mania!");
+        label.setFont(loadCustomFont("Font/Dancing_Script/static/DancingScript-Regular.ttf", Font.PLAIN, 24));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        add(label);
+        //
         Pizzaname = new JLabel("Pizza Name");
         Font pizzaFont = new Font("Arial", Font.BOLD, 18);
         Pizzaname.setFont(pizzaFont);
         Pizzaname.setBounds(30, 50, 150, 30);
         add(Pizzaname);
-        type.setBounds(150, 30, 150, 30);
+        // type
+        type.setBounds(150, 50, 150, 30);
         add(type);
         setLayout(null);
         setSize(1000, 700);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private Font loadCustomFont(String path, int style, float size) {
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream(path);
+            if (is == null) {
+                throw new RuntimeException("Font not found at the : " + path);
+            }
+            Font font = Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(style, size);
+        } catch (Exception e) {
+
+        }
     }
 
     public void actionPerformed(ActionEvent ae) {
